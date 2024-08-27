bb7dfe7fa9b19766ac227f0e138c5f0f631fa393
Arnaud JOLLY <contact@arnaud-jolly.net>
2024-08-28 00:57:33 +0200

    feature(chapter9): use Suspense for LatestInvoices

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,28 +3,19 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import {
-    fetchCardData,
-    fetchLatestInvoices
+    fetchCardData
 } from '@/app/lib/data';
 
 export default async function Page() {
 
-    const data = await Promise.all([
-        fetchLatestInvoices(),
-        fetchCardData(),
-    ]);
-
-    const [
-        latestInvoices,
-        {
-            numberOfInvoices,
-            numberOfCustomers,
-            totalPaidInvoices,
-            totalPendingInvoices
-        }
-    ] = data;
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices
+    } = await fetchCardData();
 
     return (
         <main>
@@ -41,7 +32,9 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton />}>
                     <RevenueChart />
                 </Suspense>
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<LatestInvoicesSkeleton />}>
+                    <LatestInvoices />
+                </Suspense>
             </div>
         </main>
     );
